c99aa65b140216a98f41f5042803676e66cebe2f
Matheus Rebola <matheus.rebola@outlook.com>
2024-04-05 15:55:07 -0300

    first commit

---
+++ asclepio-spring/src/main/java/br/org/saude/asclepio/AsclepioApplication.java
@@ -0,0 +1,13 @@
+package br.org.saude.asclepio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsclepioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsclepioApplication.class, args);
+	}
+
+}
---
+++ asclepio-spring/src/test/java/br/org/saude/asclepio/AsclepioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.saude.asclepio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsclepioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
