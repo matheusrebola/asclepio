9885f873490043a88d0d29b1640bae942cb1c894
matheusrebola <m.cavalcantir@outlook.com>
2024-07-18 12:15:10 -0300

    refactor: refatoração do nome do record

--- asclepio-spring/src/main/java/br/org/saude/asclepio/entities/Faturamentos.java
+++ asclepio-spring/src/main/java/br/org/saude/asclepio/entities/Faturamentos.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 import jakarta.persistence.Entity;
 
 @Entity
-public record Faturamento(
+public record Faturamentos(
 		UUID id,
 		String nome,
 		LocalDate dataNascimento,
